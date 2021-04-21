3433169b468600d946b885dcc74d2f610e2bb04c
yared.hurisa <yared.hurisa@kimetrica.com>
2021-04-21 14:23:56 -0400

    ad link

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
- 
-env_var_name = 'SECRET_KEY'
-env_var_value = 'YaredYeArsemaLij'
-if env_var_name not in os.environ:
-    os.environ[env_var_name] = env_var_value
-print(os.environ[env_var_name])
